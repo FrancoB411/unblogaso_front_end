5f76bb7ca414618555f59c711fbafcd89575bc20
Franco Barbeite <francob411@gmail.com>
2015-11-29 21:58:50 -0500

    Add different api endpoints for production and dev.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,8 @@
 import DS from "ember-data";
+import ENV from "../config/environment";
 
 export default DS.RESTAdapter.extend({
-  host: "http://localhost:4000"
+  // host: "http://localhost:4000"
+  host: ENV.APP.API_URL,
 });
 
--- config/environment.js
+++ config/environment.js
@@ -25,6 +25,8 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+
+    ENV.APP.API_URL = "http://localhost:4000";
     ENV.contentSecurityPolicy = {
       'default-src': "'none'",
       'script-src': "'self'",
@@ -49,7 +51,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.APP.API_URL = process.env.API_URL;
   }
 
   return ENV;
